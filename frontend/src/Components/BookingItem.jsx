16d2bc5010170af8c3ab0c480cd4b7c798d9bc18
Raphael0025 <rpbisla0025@gmail.com>
2023-10-20 13:33:35 +0800

    updated booking item

--- frontend/src/Components/BookingItem.jsx
+++ frontend/src/Components/BookingItem.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 const BookingItem = ({data}) => {
     return (
         <div className='d-flex flex-column flex-wrap gap-1 justify-content-start column-gap-3 booking-item w-100 rounded-3 p-3'>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Reservation ID:</span>
-                <span className=''>{data.id}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Reservation ID:</span>
+                <span className='text-truncate flex-fill '>{data.id}</span>
             </div>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Customer ID:</span>
-                <span className=''>{data.customer_id}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Customer ID:</span>
+                <span className='text-truncate flex-fill '>{data.customer_id}</span>
             </div>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Address:</span>
-                <span className=''>{data.address}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Address:</span>
+                <span className='text-truncate flex-fill '>{data.address}</span>
             </div>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Schedule:</span>
-                <span className='booking-value'>{data.schedule}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Schedule:</span>
+                <span className='booking-value flex-fill text-truncate'>{data.schedule}</span>
             </div>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Creation Date:</span>
-                <span className='booking-value'>{data.creationDate}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Creation Date:</span>
+                <span className='booking-value   flex-fill text-truncate'>{data.creationDate}</span>
             </div>
-            <div className='d-flex align-items-center gap-3'>
-                <span className='booking-label'>Contact:</span>
-                <span className=''>{data.phone}</span>
+            <div className='d-flex flex-column  align-items-start '>
+                <span className='booking-label flex-fill'>Contact:</span>
+                <span className='text-truncate flex-fill '>{data.phone}</span>
             </div>
         </div>
     )
